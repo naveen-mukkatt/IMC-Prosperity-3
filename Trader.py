36e53d0dbef0db587db99fdbca5b46ebccd6c0e8
Naveen Mukkatt <naveenmukkatt@gmail.com>
2025-04-07 17:12:35 -0500

    fixed mid-trade position updates

--- Trader.py
+++ Trader.py
@@ -121,83 +121,110 @@ logger = Logger()
 
 RR = "RAINFOREST_RESIN"
 
-class Trader:
-
+class Trader:       
     def market_take(self, product: str, bid_price_limit: int, ask_price_limit: int, position: int, limit: int, order_depth: OrderDepth, orders: list[Order]):
         # make a list of every trade you can take.
         logger.print("Executing Market Take on " + product)
-        num_buy = 0
-        num_sell = 0
+        nbuy = 0
+        nsell = 0
 
         for i in range(len(order_depth.buy_orders)):
             bid_price, bid_vol = list(order_depth.buy_orders.items())[i]
             if bid_price > bid_price_limit: 
                 # sell as many as possible here
-                sell_vol = min(limit + position - num_sell, bid_vol)
+                sell_vol = min(limit + position - nsell, bid_vol)
 
                 if sell_vol == 0:
                     break
 
                 logger.print(f"Market take: selling {sell_vol}@{bid_price}\n")
                 orders.append(Order(product, bid_price, -sell_vol))
-                num_sell += sell_vol
+                nsell += sell_vol
             
-            if position - num_sell <= -limit:
+            if position - nsell <= -limit:
                 break
 
         for i in range(len(order_depth.sell_orders)):
             ask_price, ask_vol = list(order_depth.sell_orders.items())[i]
             ask_vol *= -1
             if ask_price < ask_price_limit:
-                buy_vol = min(limit - position - num_buy, ask_vol)
+                buy_vol = min(limit - position - nbuy, ask_vol)
                 if buy_vol == 0:
                     break
                 logger.print(f"Market take: buying {buy_vol}@{ask_price}\n")
                 orders.append(Order(product, ask_price, buy_vol))
-                num_buy += buy_vol
+                nbuy += buy_vol
             
-            if position + num_buy >= limit:
+            if position + nbuy >= limit:
                 break
 
         # can try interleaving the orders to get more matches?
         # wait nvm, only one of buy/sell can exist l o l
-        return num_buy, num_sell
+        return nbuy, nsell
     
 
-    def neutralize(self, product: str, fair_val: int, position: int, num_buy: int, num_sell: int, limit: int, order_depth: OrderDepth, orders: list[Order]):
+    def neutralize(self, product: str, fair_val: int, position: int, nbuy: int, nsell: int, limit: int, order_depth: OrderDepth, orders: list[Order]):
         logger.print("Neutralizing position on " + product)
-        new_position = position + num_buy - num_sell
+        new_position = position + nbuy - nsell
         if new_position > 0:
             if len(order_depth.buy_orders) > 0:
-                best_bid_price, best_bid_vol = list(order_depth.buy_orders.items())[0]
+                best_bid_price, best_bid_vol = max(order_depth.buy_orders.items(), key=lambda x: x[0], default=(10000, new_position))
+
                 if best_bid_price >= fair_val:
-                    sell_vol = min(min(new_position, best_bid_vol), limit + new_position - num_sell) # additional param of new_position to get to net neutral
-                    logger.print(f"Neutralized {new_position} to {new_position - sell_vol} at {best_bid_price}\n")
+                    sell_vol = min(min(new_position, best_bid_vol), limit + new_position - nsell) # additional param of new_position to get to net neutral
+                    logger.print(f"Neutralizing {new_position} to {new_position - sell_vol} at {best_bid_price}\n")
                     orders.append(Order(product, best_bid_price, -sell_vol))
+                    nsell += sell_vol
 
         elif new_position < 0:
             if len(order_depth.sell_orders) > 0:
-                best_ask_price, best_ask_vol = list(order_depth.sell_orders.items())[0] # maybe turn into loop to keep checking other sell orders?
+                best_ask_price, best_ask_vol = max(order_depth.sell_orders.items(), key = lambda x: x[0], default=(10000, new_position))
                 if best_ask_price <= fair_val:
-                    buy_vol = min(min(-new_position, -best_ask_vol), limit - position - num_buy)
-                    logger.print(f"Neutralized {new_position} to {new_position + buy_vol} at {best_ask_price}")
+                    buy_vol = min(min(-new_position, -best_ask_vol), limit - new_position - nbuy)
+                    logger.print(f"Neutralizing {new_position} to {new_position + buy_vol} at {best_ask_price}")
                     orders.append(Order(product, best_ask_price, buy_vol))
+                    nbuy += buy_vol
+
+        return nbuy, nsell
+
+    def market_make(self, product: str, fair_val: int, position: int, bid_edge: float, ask_edge: float, nbuy: int, nsell: int, limit: int, order_depth: OrderDepth, orders: list[Order], slight_undercut: bool=True, bid_target=9999, ask_target=10001):
+        logger.print("Starting MM Algorithm: " + product)
+
+        if slight_undercut:    
+            bid_target = max([bid for bid, vol in order_depth.buy_orders.items()  if bid < fair_val - bid_edge], default=fair_val - bid_edge - 1) + 1
+            ask_target = min([ask for ask, vol in order_depth.sell_orders.items() if ask > fair_val + ask_edge], default=fair_val + ask_edge + 1) - 1
+        bid_vol = limit - position - nbuy
+        ask_vol = limit + position - nsell
+
+        logger.print(f"BID {bid_target}@{bid_vol}, ASK {ask_target}@{ask_vol}.  Position/nbuy/nsell = {position}/{nbuy}/{nsell}\n")
+        orders.append(Order(product, bid_target, bid_vol))
+        orders.append(Order(product, ask_target, -ask_vol))
+        nbuy += bid_vol
+        nsell += ask_vol
+
+        return nbuy, nsell
 
 
-    
     def executor(self, order_depth: OrderDepth, product: str, position: int): # add product to execute set of strategies
         traderData = product
         orders: List[Order] = []
 
         POS_LIMIT = {"KELP": 50, RR: 50}
         RR_fair_val = 10000
+        RR_bid_edge = 1
+        RR_ask_edge = 1
 
         if product == RR:
+            lim = POS_LIMIT[product]
             logger.print("Executing RR Strategy")
-            nbuy, nsell = self.market_take(product, RR_fair_val - 1, RR_fair_val + 1, position, POS_LIMIT[product], order_depth, orders)
-            logger.print("Market taking done.")
-            self.neutralize(product, RR_fair_val, position, nbuy, nsell, POS_LIMIT[product], order_depth, orders)
+            nbuy, nsell = self.market_take(product, RR_fair_val - 1, RR_fair_val + 1, position, lim, order_depth, orders)
+            logger.print(f"Market taking done. Nbuy = {nbuy}, Nsell = {nsell}\n")
+            nbuy, nsell = self.neutralize(product, RR_fair_val, position, nbuy, nsell, lim, order_depth, orders)
+            logger.print(f"Market neutralization done. Nbuy = {nbuy}, Nsell = {nsell}\n")
+            nbuy, nsell = self.market_make(product, RR_fair_val, position, RR_bid_edge, RR_ask_edge, nbuy, nsell, lim, order_depth, orders, slight_undercut=True, bid_target=9998, ask_target=10002)
+            logger.print(f"Market making done. Nbuy = {nbuy}, Nsell = {nsell}\n")
             logger.print("RR Strategy done.")
+            
 
         
         return orders, traderData
